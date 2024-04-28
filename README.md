7252699641c40be92b88c09df8385a5de11766ab
triangletrianglesquareplusx <preslavgetov@gmail.com>
2024-04-28 19:24:54 +0300

    update readme

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ In the command line, type - ```cd inscribe```, ```npm run install```, ```npm run
 ## Helpers or things I have used while working on the project:
 
 1. [SxPropsIssue](https://github.com/mui/material-ui/issues/27564)
+2. [uuidv4 for key mapping](https://www.npmjs.com/package/uuid). After all we have no guarantee a user will input only unique task names
+3. [react-toastify for visual ques for the user](https://www.npmjs.com/package/react-toastify)
+4. [React Hook Form, my favorite package for forms](https://react-hook-form.com/)
--- src/components/single-note/SingleNote.tsx
+++ src/components/single-note/SingleNote.tsx
@@ -5,19 +5,25 @@ import Typography from "@mui/material/Typography";
 type SingleNoteProps = {
   title: string;
   description: string;
-  date: string
+  date: string;
 };
 
 const SingleNote = ({ title, description, date }: SingleNoteProps) => {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card
+      sx={{
+        minWidth: "70%",
+        backgroundColor: "inherit",
+        color: "white",
+        border: "1px solid gray",
+        marginBottom: '1em'
+      }}
+    >
       <CardContent>
         <Typography variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2">
-          {description}
-        </Typography>
+        <Typography variant="body2">{description}</Typography>
         <Typography variant="h5" component="div">
           {date}
         </Typography>
